553d3378f310d9ae7808816fc2fc571ddd020f73
Hermesis <hermesiss@gmail.com>
2019-07-01 14:56:26 +0400

    Add execution time display

--- practice-opengl-analogue-csharp/MainWindow.xaml.cs
+++ practice-opengl-analogue-csharp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using Extensions;
 using Color = System.Drawing.Color;
@@ -15,6 +17,7 @@ namespace practice_opengl_analogue_csharp {
         private const int Size = 512;
         private readonly Image _image;
         private readonly Random _random = new Random();
+        private Bitmap _bitmap;
 
         public MainWindow() {
             InitializeComponent();
@@ -22,28 +25,49 @@ namespace practice_opengl_analogue_csharp {
             RenderOptions.SetBitmapScalingMode(RenderImage, BitmapScalingMode.HighQuality);
         }
 
-        private void DrawDot() {
-            var bitmap = new Bitmap(Size, Size, PixelFormat.Format24bppRgb);
-            bitmap.SetPixel(_random.Next(Size), _random.Next(Size), Color.Red);
-
-            _image.Source = bitmap.BitmapImage();
+        private void DrawDots() {
+            for (var i = 0; i < 50; i++) _bitmap.SetPixel(_random.Next(Size), _random.Next(Size), Color.Red);
         }
 
         private void DrawLines() {
-            var bitmap = new Bitmap(Size, Size, PixelFormat.Format24bppRgb);
-            bitmap = bitmap.DrawLine(new Vector2(30, 18), new Vector2(134, 428), Color.Green);
-            bitmap = bitmap.DrawLine(new Vector2(134 + 5, 428), new Vector2(30 + 5, 18), Color.Red);
-            bitmap = bitmap.DrawLine(new Vector2(_random.Next(Size), _random.Next(Size)),
-                new Vector2(_random.Next(Size), _random.Next(Size)), Color.Blue);
-            _image.Source = bitmap.BitmapImage();
-        }
-        
-        private void newTextureBtn_Click(object sender, RoutedEventArgs e) {
-            DrawDot();
+            _bitmap = _bitmap.DrawLine(new Vector2(30, 18), new Vector2(134, 428), Color.Green);
+            _bitmap = _bitmap.DrawLine(new Vector2(134 + 5, 428), new Vector2(30 + 5, 18), Color.Red);
+            for (var i = 0; i < 50; i++)
+                _bitmap = _bitmap.DrawLine(new Vector2(_random.Next(Size), _random.Next(Size)),
+                    new Vector2(_random.Next(Size), _random.Next(Size)), Color.Blue);
         }
 
-        private void DrawLineBtn_OnClickBtn_Click(object sender, RoutedEventArgs e) {
-            DrawLines();
+        private void MakeTexture(object sender, RoutedEventArgs e) {
+            _bitmap = new Bitmap(Size, Size, PixelFormat.Format24bppRgb);
+
+            Enum.TryParse(((Button) sender).CommandParameter.ToString(), out ActionType t);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            switch (t) {
+                case ActionType.None:
+                    break;
+                case ActionType.DrawDot:
+                    DrawDots();
+                    break;
+                case ActionType.DrawLines:
+                    DrawLines();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            TextMsBlock.Text = $"Made in {1000f * stopwatch.ElapsedTicks / Stopwatch.Frequency} ms";
+            stopwatch.Stop();
+
+            _image.Source = _bitmap.BitmapImage();
         }
+    }
+
+    public enum ActionType {
+        None,
+        DrawDot,
+        DrawLines
     }
 }
