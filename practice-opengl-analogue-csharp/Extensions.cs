71274a375be331a4b520d22b8effe38cbd933b57
Hermesis <hermesiss@gmail.com>
2019-07-01 15:55:58 +0400

    Add vectors from NuGet instead of custom

--- practice-opengl-analogue-csharp/Extensions.cs
+++ practice-opengl-analogue-csharp/Extensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Numerics;
 using System.Windows.Media.Imaging;
 using practice_opengl_analogue_csharp;
 
